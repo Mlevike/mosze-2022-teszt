d560ad3a377b4b4bd47feeb19933428dd9d8c17e
Márton Levente <38218622+Mlevike@users.noreply.github.com>
2022-09-09 09:25:11 +0200

    Bugs commented in code

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //N_ELEMENTS helyett itt NELEMENTS szerepel
+    std::cout << '1-100 ertekek duplazasa' //String jelölő helyett karakterjelölő van, illetve hiányzik a sor végi lezárás
+    for (int i = 0;) //Hiányzik a  kifejezés a for ciklusból
     {
         b[i] = i * 2;
     }
     for (int i = 0; i; i++)
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //Hiányzik a sor végi lezárás
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS, i++) //; jel helyett , jel lett írva a for ciklusnál
     {
-        atlag += b[i]
+        atlag += b[i] //Hiányzik a sor végi lezárás
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
